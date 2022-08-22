ebcec1fdeed404ff7939f2c480538e0d4198b7c0
gregz014 <gstrillanes@gmail.com>
2022-08-22 18:31:45 +0800

    Responsive with thank you page

---
+++ script.js
@@ -0,0 +1,36 @@
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const dots = Array.from(document.querySelectorAll(".dot"));
+
+let slideIndex = 1;
+
+function plusSlides(e) {
+  let num;
+
+  if (e.target === prevBtn) num = -1;
+  if (e.target === nextBtn) num = 1;
+
+  showSlides((slideIndex += num));
+}
+
+function showSlides(n) {
+  const slides = Array.from(document.querySelectorAll(".slide"));
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  slides.forEach((slide) => slide.classList.remove("is-active"));
+  dots.forEach((dot) => dot.classList.remove("is-active"));
+
+  slides[slideIndex - 1].classList.add("is-active");
+  dots[slideIndex - 1].classList.add("is-active");
+}
+
+prevBtn.addEventListener("click", plusSlides);
+nextBtn.addEventListener("click", plusSlides);
+dots.forEach((dot) => dot.addEventListener("click", currentSlide));
+
+
+
+
+
